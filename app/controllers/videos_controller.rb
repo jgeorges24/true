e186aa03966177201ea417078164bd8c61954d1d
JEAN-BAPTISTE <jgeorges24@icloud.com>
2021-09-09 15:13:24 -0400

    configuring the crud with back end models

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,2 +1,14 @@
 class VideosController < ApplicationController
+
+    def index
+
+        videos = Video.all
+        render json:.to_json(:include => :rapper)
+
+
+    end
+
+
+
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+# z1 = Rapper.new("The migos")
+# z2 = 
+# z3 = 
