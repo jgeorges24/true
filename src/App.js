836ea6cd83bdf8a688273ac7975271843f9ba434
JEAN-BAPTISTE <jgeorges24@icloud.com>
2021-09-10 09:49:45 -0400

    configuring fornt end routes such as home and created the Home.jsx file in components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter as Router, Route} from "react-router-dom"
+import { Switch } from "react-router"
 
 
 function App() {
   return (
     <div>
-      <h1>hello papa!!!</h1>
+      <Router>
+        <div>
+          <Switch>
+            <Route path="/home" render={(routerprops) => <Home/>}/>
+            <Route path="/tracks" />
+            <Route path="tracks/new" />
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
