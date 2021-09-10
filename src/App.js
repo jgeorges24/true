68a06a7892152a76fa16760f96c288481c23c80c
JEAN-BAPTISTE <jgeorges24@icloud.com>
2021-09-10 10:13:42 -0400

    configuring front end components nav bar and home, configuring routes for home and nav bar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Route} from "react-router-dom"
+import {BrowserRouter as Router, Route} from "React-router-dom"
 import { Switch } from "react-router"
+import Home from "./components/Home"
 
 
 function App() {
@@ -10,7 +11,7 @@ function App() {
       <Router>
         <div>
           <Switch>
-            <Route path="/home" render={(routerprops) => <Home/>}/>
+            <Route path="/home" render={(routerProps) => <Home {...routerProps} />}/>
             <Route path="/tracks" />
             <Route path="tracks/new" />
           </Switch>
