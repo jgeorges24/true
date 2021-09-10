9de42358447e35c39b4942e124fa6771626fb56f
JEAN-BAPTISTE <jgeorges24@icloud.com>
2021-09-10 09:21:21 -0400

    added rootreducer configuring the index.js to import reducers

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {createStore} from "redux"
+import { createStore } from "redux"
+import {rootReducer} from "./reducers/rootReducer"
 
-const store = createStore()
+const store = createStore(rootReducer)
 
 
 ReactDOM.render(
