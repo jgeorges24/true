364202c6ea37d4232ac1985330ec8afcce40900e
JEAN-BAPTISTE <jgeorges24@icloud.com>
2021-09-11 12:59:56 -0400

    created form for videos in vudeiForm.jsx /compnents

--- src/components/VideoForm.jsx
+++ src/components/VideoForm.jsx
@@ -4,17 +4,22 @@ import  {addVideo} from "../actions/videos"
 
 export class VideoForm extends Component {
     
-    
-    
+    state ={
+        videos: []
+    }
     
     render() {
         return (
             <div>
                 <form onSubmit={this.handleSubmit}>
                     <lable htmlFor="song">Song</lable>
-                    <input onChange={this.handleChange} type="text" name="song" id="" value={this.state.title}/>
+                    <input onChange={this.handleChange} type="text" name="song" id="" value={this.state.song}/>
                     <br></br>
                     <lable htmlFor="name">name of rapper</lable>
+                    <input onChange={this.handleChange} type="text" name="name" id="" value={this.state.name}/>
+                    <br></br>
+                    <button>add music video</button>
+                   
                 </form>
             </div>
         )
