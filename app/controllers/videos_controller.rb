0da28c83a9088e1ae3bb79f8883cc02763ac6c77
JEAN-BAPTISTE <jgeorges24@icloud.com>
2021-09-11 11:37:23 -0400

    made changes to account for create route for video form

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -8,6 +8,17 @@ class VideosController < ApplicationController
 
     end
 
+    #create those routes to account 
+    def create 
+
+        video = Video.new
+        render json: videos.to_json(:include => :rapper)
+
+    end
+
+
+
+    private
 
 
 
